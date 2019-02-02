3005a66b63a45dceb9c8d943214da4a6f7afcade
And-Rasta <avangaea@gmail.com>
2019-02-01 18:13:29 -0600

    Adding gitignore, friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+
+
+var friends = [{
+    "name": "Andrasta Seatts",
+    "photo": "https://media.licdn.com/dms/image/C4E03AQFPanSZmG2RMw/profile-displayphoto-shrink_200_200/0?e=1552521600&v=beta&t=GhXxeLqPpMw7x6O7npbL1VKkP6Ipxs-Vewed3KCu2ek",
+    "scores": [
+        5,
+        5,
+        5,
+        1,
+        1,
+        3,
+        5,
+        1,
+        4,
+        5,
+    ],   
+}];
+
+module.exports = friends;
